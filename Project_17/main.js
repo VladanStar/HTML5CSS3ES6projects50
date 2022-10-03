0965e7af587a4bdf0e104ce2470919e080464481
VladanStar <vladancupric@gmail.com>
2022-10-03 13:35:46 +0200

    Add

--- Project_17/main.js
+++ Project_17/main.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7a42535d07bab5cf8947d3b0291eb0d3&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
